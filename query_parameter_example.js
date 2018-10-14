75ae04ae7f6894ba915b61f258bc86a1b21137a0
Angeline <atan@thoughtworks.com>
2018-10-15 02:17:08 +0800

    Add example for parsing query params section in git book

---
+++ query_parameter_example.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+const data = [
+  {
+    id: 1,
+    type: "FRUIT",
+    name: "Banana",
+    color: "yellow"
+  },
+  {
+    id: 2,
+    type: "FRUIT",
+    name: "Tomato",
+    color: "red"
+  },
+  {
+    id: 3,
+    type: "VEGETABLE",
+    name: "Broccoli",
+    color: "green"
+  },
+  {
+    id: 4,
+    type: "VEGETABLE",
+    name: "Red pepper",
+    color: "red"
+  }
+];
+
+app.get("/food", (req, res) => {
+  console.log(req.query);
+
+  const results = data
+    // Using if statements
+    .filter(item => {
+      if (req.query.type) {
+        return item.type === req.query.type;
+      }
+
+      return true;
+    })
+    // Using the ternary operator
+    .filter(item => (req.query.name ? item.name === req.query.name : true))
+    .filter(item => (req.query.color ? item.color === req.query.color : true));
+
+  res.json(results);
+});
+
+
+const server = app.listen(3000, function() {
+    console.log("Application started....");
+});
+  
